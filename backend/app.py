5e94e5719f55870b6f1bbfe9abd3dd73fde90aa6
MaxSmeets <maxsmeets1996@gmail.com>
2026-01-08 11:29:56 +0100

    menu + routing

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def admin_dashboard():
+    return render_template("admin.html", active_page="admin")
+
+@app.route("/projects")
+def projects_overview():
+    return render_template("projects.html", active_page="projects")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)

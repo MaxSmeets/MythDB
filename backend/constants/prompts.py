736eedd0c324eef46caf07cf645c65efd3daabb4
MaxSmeets <maxsmeets1996@gmail.com>
2026-01-28 13:18:29 +0100

    Added new article prompts

--- backend/constants/prompts.py
+++ backend/constants/prompts.py
@@ -1,6 +1,7 @@
 """Prompt definitions for article types."""
 
 DEFAULT_PROMPTS_PER_ARTICLE_TYPE = [
+    # NPC prompts
     {
         "article_type_key": "npc",
         "prompt_key": "age",
@@ -28,5 +29,42 @@ DEFAULT_PROMPTS_PER_ARTICLE_TYPE = [
         "prompt_text": "Profession",
         "prompt_type": "select",
         "prompt_linked_type_key": "profession",
+    },
+    # Settlement prompts
+    {
+        "article_type_key": "settlement",
+        "prompt_key": "population",
+        "prompt_text": "Population",
+        "prompt_type": "text",
+        "prompt_linked_type_key": None,
+    },
+    {
+        "article_type_key": "settlement",
+        "prompt_key": "ruling_entity",
+        "prompt_text": "Ruling Entity",
+        "prompt_type": "select",
+        "prompt_linked_type_key": "faction",
+    },
+    # Faction prompts
+    {
+        "article_type_key": "faction",
+        "prompt_key": "founding_date",
+        "prompt_text": "Founding Date",
+        "prompt_type": "text",
+        "prompt_linked_type_key": None,
+    },
+    {
+        "article_type_key": "faction",
+        "prompt_key": "headquarters",
+        "prompt_text": "Headquarters",
+        "prompt_type": "select",
+        "prompt_linked_type_key": "settlement",
+    },
+    {
+        "article_type_key": "faction",
+        "prompt_key": "leader",
+        "prompt_text": "Leader",
+        "prompt_type": "select",
+        "prompt_linked_type_key": "npc",
     }
 ]
